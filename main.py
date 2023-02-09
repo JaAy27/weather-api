168701dea09bb4d648ba6b2c9a33d9fa89b6c857
JaAy27 <jnax27@hotmail.com>
2023-02-09 12:35:41 -0500

    Added more url endpoints

--- main.py
+++ main.py
@@ -10,12 +10,28 @@ def home():
     return render_template("home.html", data=stations.to_html())
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    filename = "data_small/TG_STAID00000" + str(station).zfill(6) + ".txt"
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
     temperature = df.loc[df['    DATE'] == date]["   TG"].squeeze()/10
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)  ## parse_dates=['    DATE'] converts the date col to date type values
+    df["    DATE"] = df['    DATE'].astype(str)
+    result = df[df['    DATE'].str.startswith(str(year))].to_dict(orient="records")
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
